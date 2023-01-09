0088920471e7a277be82227996b446dbf95b2a23
Romen Rodriguez-Gil <rrodriguezgil@turo.com>
2023-01-09 19:23:29 +0000

    feat: add nock to allowed modules in node/no-unpublished-import
    to prevent eslint from erroring in test usages with "'nock' is not published.(node/no-unpublished-import)"

--- index.js
+++ index.js
@@ -64,7 +64,7 @@ module.exports = {
     "node/no-unpublished-import": [
       "error",
       {
-        allowModules: ["@jest/globals"],
+        allowModules: ["@jest/globals", "nock"],
       },
     ],
     "node/no-unsupported-features/es-syntax": "off",
