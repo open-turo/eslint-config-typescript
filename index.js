eda64316bd6cf3c65defa396c675166aecae8670
Michael Altamirano <michaeljaltamirano@gmail.com>
2025-01-29 05:22:50 +0000

    chore: turn off @typescript-eslint/no-deprecated

--- index.js
+++ index.js
@@ -103,6 +103,8 @@ module.exports = {
         prefer: "type-imports",
       },
     ],
+    /** Included as part of `strict-type-checked`, but nothing we want to enforce. */
+    "@typescript-eslint/no-deprecated": "off",
     /** Prefers `import type {}` syntax over `import { type }` if all imports are type-only */
     "@typescript-eslint/no-import-type-side-effects": "error",
     "@typescript-eslint/no-unused-vars": [
