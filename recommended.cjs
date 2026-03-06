17a243d533e746e54dcbc691a1b215756370ae89
Michael Altamirano <michaeljaltamirano@gmail.com>
2026-03-06 00:49:36 +0000

    fix(index.cjs): apply strict type checked config in flat config, legacy config discrepancy

--- recommended.cjs
+++ recommended.cjs
@@ -5,7 +5,6 @@ module.exports = {
   },
   extends: [
     "eslint:recommended",
-    "plugin:@typescript-eslint/recommended-type-checked",
     "plugin:@typescript-eslint/strict-type-checked",
     "plugin:import/recommended",
     "plugin:import/typescript",
