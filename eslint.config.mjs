01aef0b7f02ad4712f87bd0f89da577dd34c7685
Michael Altamirano <michaeljaltamirano@gmail.com>
2026-03-09 21:46:30 +0000

    fix: reflect typescript: true in snapshots for more canonical use-case, remove no-loss-of-precision

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,4 +1,4 @@
 import openTuroTypescriptConfig from "./index.mjs";
 
 // eslint-disable-next-line import/no-default-export
-export default openTuroTypescriptConfig({ typescript: false });
+export default openTuroTypescriptConfig({ typescript: true });
